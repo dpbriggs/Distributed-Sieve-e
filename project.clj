9ce12a6be7fc23fb87e44643135bc41f136c65b9
David <imultiplay123@gmail.com>
2015-01-14 13:38:00 -0500

    Update project.clj

--- project.clj
+++ project.clj
@@ -3,8 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.gearswithingears/async-sockets "0.1.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]]
   :main ^:skip-aot mail-sieve-e.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
